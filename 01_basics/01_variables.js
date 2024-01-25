a684d485c5ac395feb0bf3619a3777bddd24ebe8
Hemanth044 <123920004+Hemanth-044@users.noreply.github.com>
2024-01-25 12:29:00 +0000

    config changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 144553
+let accountEmail="abcd@gmail.com"
+accountCity="Hyderabad"
+let accountState;
+
+console.table([accountId,accountEmail,accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+let h=null
+
+//alert(3+3) // we are using nodejs and not browser
+
+// number=> 2 to power 53
+// bigint
+// string=""
+// boolean
+// null=> standalone value not zero
+//undefined=>
+// symbol=
+
+
+//object=
+
+console.log(typeof h);
