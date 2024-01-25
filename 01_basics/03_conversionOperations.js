deabf17f55a9698b7aa1d169e5b364200894f2af
Hemanth044 <123920004+Hemanth-044@users.noreply.github.com>
2024-01-25 13:26:50 +0000

    config changes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score= 33
+ 
+console.log(typeof score);
+console.log(score);
+
+let strofVlaue= String(score)
+console.log(strofVlaue);
+console.log(typeof strofVlaue);
+
+// 33 => number
+// "33abc"=> Nan
+// null=> 0
+// undefined => Nan
+//true=> 1
+// False=>0
+// ""=> flase
+// "sc"=> true
